a3d4a7c4063a965182eba15b87dc4020adc0d4e9
aaifox <36447732+aaifox@users.noreply.github.com>
2019-03-04 18:14:46 +0800

    Add files via upload

---
+++ jackal_simulator/jackal_gazebo/control_1.cpp
@@ -0,0 +1,34 @@
+#include "ros/ros.h"
+//#include "std_msgs/String.h"
+#include <geometry_msgs/Twist.h>
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "control_1");
+    ros::Nodehandle CN;
+    ros::Publisher control_pub = CN.advertise<geometry_msg::Twist>("twist_marker_server/cmd_vel", 1);
+//    ros::Publisher control_pub = CN.advertise<geometry_msg::Twist>("jackal_velocity_controller/cmd_vel", 1);
+    ros::Rate loop_rate(5);
+
+    float count = 1;
+    while(ros::ok())
+    {
+        geometry_msgs::Twist cmd_vel;
+
+        cmd_vel.linear.x = count;
+        cmd_vel.linear.y = 0.0;
+        cmd_vel.angular.z = 0.0;
+
+        ROS_INFO("velocity (x,y,w): %f, %f, %f", cmd_vel.linear.x, cmd_vel.linear.y, cmd_vel.angular.z);
+
+        control_pub.publish(cmd_vel);
+
+        ros::spinOnce();
+
+        loop_rate.sleep();
+
+        count = count+0.1;        
+    }
+
+    return 0;
+}
